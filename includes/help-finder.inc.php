b94eb4abb308f18d0e1850b89b20a018bcd53487
Alex <103012401+AlexanderPadayachee@users.noreply.github.com>
2022-05-24 19:38:34 +1000

    Add files via upload

--- includes/help-finder.inc.php
+++ includes/help-finder.inc.php
@@ -1,4 +1,18 @@
 <?php 
 	require_once 'dbh.inc.php';
 	require_once 'functions.inc.php';
+	if(isset($_POST["updateService"])){
+		$serviceID = $_POST["serviceID"];
+		$profID = $_POST["ProfID"];
+		assignProf($conn, $serviceID, $profID);1
+		#echo("check");
+		header("location: ../help-finder.php");
+	}
+	if(isset($_POST["FinishService"])){
+		$serviceID = $_POST["serviceID"];
+		finishService($conn, $serviceID);
+		#echo("check");
+		header("location: ../help-finder.php");
+	}
+	
 	
